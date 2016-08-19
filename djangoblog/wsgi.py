d8a2409a94f455dd1d70fdfc5f4a394eac31f694
Brian Lee <739142578@qq.com>
2016-08-19 19:54:33 +0800

    Add WSGI config to wsgi.py

--- djangoblog/wsgi.py
+++ djangoblog/wsgi.py
@@ -7,7 +7,12 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
+ #set VirtualEnv
+# sys.path.append("/path/to/site-packages")  
+
+# Set Site Address
+# sys.path.append("/path/to/site")
 
 from django.core.wsgi import get_wsgi_application
 
