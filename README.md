1fbbb6d3e6e5b9e77cf1f0e5f40db33384305ce4
LiTingjun <739142578@qq.com>
2016-12-19 15:49:51 +0800

    Use npm, add vue and materialize-css

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-#Blog Powered by Django
+# Blog Powered by Django
+
+[![Build Status](https://travis-ci.org/BrianLTJ/djangoblog.svg?branch=master)](https://travis-ci.org/BrianLTJ/djangoblog)
+
+## V2 - Currently in building
+
+Restful APIs and Loading
--- control/views.py
+++ control/views.py
@@ -7,9 +7,9 @@ import markdown2
 import time
 from datetime import datetime, date
 
-from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth.mixins import LoginRequiredMixin
+# from django.contrib.auth import authenticate, login, logout
+# from django.contrib.auth.decorators import login_required
+# from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 def GetIDBasedOnTime():
@@ -23,30 +23,31 @@ def login_index(request):
 
 def login_handler(request):
     if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return HttpResponseRedirect('/admin')
-        else:
-            return HttpResponseRedirect('/admin/login')
+        # username = request.POST['username']
+        # password = request.POST['password']
+        # user = authenticate(username=username, password=password)
+        # if user is not None:
+        #     login(request, user)
+        #     return HttpResponseRedirect('/admin')
+        # else:
+        #     return HttpResponseRedirect('/admin/login')
+        return HttpResponseRedirect('/admin/login')
     else:
         return Http404
 
 
-@login_required(login_url='/admin/login')
+# @login_required(login_url='/admin/login')
 def logout_handler(request):
-    logout(request)
+    # logout(request)
     return HttpResponseRedirect('/admin')
 
 
-@login_required(login_url='/admin/login')
+# @login_required(login_url='/admin/login')
 def Index(request):
     return render(request, 'control/index/index.html')
 
 
-@login_required(login_url='/admin/login')
+# @login_required(login_url='/admin/login')
 def ArticleList(request, page_type):
     if page_type == 'all':
         article_list = Article.objects.all()
@@ -63,7 +64,7 @@ def ArticleList(request, page_type):
     return render(request, 'control/article/list.html', context)
 
 
-@login_required(login_url='/admin/login')
+# @login_required(login_url='/admin/login')
 def ArticleAdd(request):
     category = Category.objects.all()
     tag = Tag.objects.all()
@@ -71,7 +72,7 @@ def ArticleAdd(request):
     return render(request, 'control/article/content.html', context)
 
 
-@login_required(login_url='/admin/login')
+# @login_required(login_url='/admin/login')
 def ArticleAddHandler(request):
     if request.method == 'POST':
         article = Article()
@@ -90,7 +91,7 @@ def ArticleAddHandler(request):
     return HttpResponseRedirect('/admin/article/all')
 
 
-class ArticleEdit(LoginRequiredMixin, DetailView):
+class ArticleEdit(DetailView):
     login_url = '/admin/login'
     redirect_field_name = 'redirect_to'
     model = Article
@@ -110,7 +111,7 @@ class ArticleEdit(LoginRequiredMixin, DetailView):
         return super(ArticleEdit, self).get_context_data(**kwargs)
 
 
-@login_required(login_url='/admin/login')
+# @login_required(login_url='/admin/login')
 def ArticleEditHandler(request):
     if request.method == 'POST':
         article = Article.objects.get(id=request.POST.get('id'))
@@ -128,13 +129,13 @@ def ArticleEditHandler(request):
     return HttpResponseRedirect('/admin/article/all')
 
 
-@login_required(login_url='/admin/login')
+# @login_required(login_url='/admin/login')
 def ArticleDelHandler(request, article_id):
     Article.objects.get(id=article_id).delete()
     return HttpResponseRedirect('/admin/article/all')
 
 
-@login_required(login_url='/admin/login')
+# @login_required(login_url='/admin/login')
 def ArticleRecycleHandler(request, article_id):
     article = Article.objects.get(id=article_id)
     article.status = 'r'
@@ -142,7 +143,7 @@ def ArticleRecycleHandler(request, article_id):
     return HttpResponseRedirect('/admin/article/recycle')
 
 
-@login_required(login_url='/admin/login')
+# @login_required(login_url='/admin/login')
 def ArticleRestoreToDraftHandler(request, article_id):
     article = Article.objects.get(id=article_id)
     article.status = 'd'
@@ -150,7 +151,7 @@ def ArticleRestoreToDraftHandler(request, article_id):
     return HttpResponseRedirect('/admin/article/draft')
 
 
-@login_required(login_url='/admin/login')
+# @login_required(login_url='/admin/login')
 def ArticlePublishHandler(request, article_id):
     article = Article.objects.get(id=article_id)
     article.status = 'p'
@@ -159,7 +160,7 @@ def ArticlePublishHandler(request, article_id):
 
 
 # 文章置顶
-@login_required(login_url='/admin/login')
+# @login_required(login_url='/admin/login')
 def ArticleTopHandler(request, article_id):
     article = Article.objects.get(id=article_id)
     article.topped = True
@@ -168,7 +169,7 @@ def ArticleTopHandler(request, article_id):
 
 
 # 文章取消置顶
-@login_required(login_url='/admin/login')
+# @login_required(login_url='/admin/login')
 def ArticleUnTopHandler(request, article_id):
     article = Article.objects.get(id=article_id)
     article.topped = False
@@ -181,7 +182,7 @@ def ArticleUnTopHandler(request, article_id):
 '''
 
 
-@login_required(login_url='/admin/login')
+# @login_required(login_url='/admin/login')
 def CategoryList(request):
     template_name = 'control/attr/list.html'
 
@@ -190,7 +191,7 @@ def CategoryList(request):
     return render(request, template_name, context)
 
 
-@login_required(login_url='/admin/login')
+# @login_required(login_url='/admin/login')
 def CategoryAddHandler(request):
     if request.method == 'POST':
         cate = Category()
@@ -201,7 +202,7 @@ def CategoryAddHandler(request):
     return HttpResponseRedirect('/admin/attr/category')
 
 
-@login_required(login_url='/admin/login')
+# @login_required(login_url='/admin/login')
 def CategoryEditHandler(request):
     if request.method == 'POST':
         cate = Category.objects.get(id=request.POST.get('id'))
@@ -210,14 +211,14 @@ def CategoryEditHandler(request):
     return HttpResponseRedirect('/admin/attr/category')
 
 
-@login_required(login_url='/admin/login')
+# @login_required(login_url='/admin/login')
 def CategoryDelHandler(request):
     if request.method == 'POST':
         Category.objects.get(id=request.POST.get('id')).delete()
     return HttpResponseRedirect('/admin/attr/category')
 
 
-@login_required(login_url='/admin/login')
+# @login_required(login_url='/admin/login')
 def TagList(request):
     template_name = 'control/attr/list.html'
 
@@ -226,7 +227,7 @@ def TagList(request):
     return render(request, template_name, context)
 
 
-@login_required(login_url='/admin/login')
+# @login_required(login_url='/admin/login')
 def TagAddHandler(request):
     if request.method == 'POST':
         tag = Tag()
@@ -237,7 +238,7 @@ def TagAddHandler(request):
     return HttpResponseRedirect('/admin/attr/tag')
 
 
-@login_required(login_url='/admin/login')
+# @login_required(login_url='/admin/login')
 def TagEditHandler(request):
     if request.method == 'POST':
         tag = Tag.objects.get(id=request.POST.get('id'))
@@ -246,10 +247,8 @@ def TagEditHandler(request):
     return HttpResponseRedirect('/admin/attr/tag')
 
 
-@login_required(login_url='/admin/login')
-def TagDelHandler(request):
+# @login_required(login_url='/admin/login')
+def TagDelHandler(request, method='POST'):
     if request.method == 'POST':
         Tag.objects.get(id=request.POST.get('id')).delete()
     return HttpResponseRedirect('/admin/attr/Tag')
-
-
