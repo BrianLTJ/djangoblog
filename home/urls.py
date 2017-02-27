a25677542345ec933a9e3de31daebe4c1e54b918
LiTingjun <739142578@qq.com>
2017-02-27 23:42:57 +0800

    change requirements

--- home/urls.py
+++ home/urls.py
@@ -4,5 +4,4 @@ from home import views
 
 urlpatterns = [
     url(r'^$', views.index, name='blog_index'),
-    url(r'^', views.view_post, name='blog_view_post'),
 ]
--- home/views.py
+++ home/views.py
@@ -2,8 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'home/index/index.html')
+    return render(request, 'home/index.html')
 
-
-def view_post(request):
-    return render(request, 'home/post/detail.html')
